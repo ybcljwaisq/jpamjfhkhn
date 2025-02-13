878265acfd135fca1b10d3917d80c728359f8de7
ybcljwaisq <ybcljwaisq@no.existing.email.com>
2025-02-13 13:46:48 +0100

    .

--- a2rl_OpenPCDet/docker_train.sh
+++ a2rl_OpenPCDet/docker_train.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 docker run \
-    -v /home/ubuntu/a2rl-perception-data/dataset_a2rl_new/data/v1.0-mini/:/opencv/a2rl_OpenPCDet/data/nuscenes/v1.0-mini \
+    -v $PWD/../data/v1.0-mini/:/opt/ar_dataset/a2rl_OpenPCDet/data/nuscenes/v1.0-mini \
     -v $PWD/output:/opencv/a2rl_OpenPCDet/output \
     --gpus all -it ar_dataset
+
 
     # python3 -m pcdet.datasets.nuscenes.nuscenes_dataset --func create_nuscenes_infos     --cfg_file tools/cfgs/dataset_configs/nuscenes_dataset_a2rl.yaml     --version v1.0-mini
     # python3 train.py --cfg_file cfgs/nuscenes_models/cbgs_pillar0075_res2d_centerpoint_a2rl.yaml
